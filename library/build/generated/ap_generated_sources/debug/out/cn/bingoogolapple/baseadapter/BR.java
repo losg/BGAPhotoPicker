aff2d713090c68ef353fc10265db158bdb9f0b50
losg <wangyongxi8653@yundaex.com>
2021-08-04 16:52:58 +0800

    first commit

---
+++ library/build/generated/ap_generated_sources/debug/out/cn/bingoogolapple/baseadapter/BR.java
@@ -0,0 +1,13 @@
+package cn.bingoogolapple.baseadapter;
+
+public class BR {
+  public static int _all = 0;
+
+  public static int uiHandler = 1;
+
+  public static int viewHolder = 2;
+
+  public static int statusModel = 3;
+
+  public static int model = 4;
+}
